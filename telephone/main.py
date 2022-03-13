ca20442a543d9cad56ab08de93f586ced99d9830
voidful <voidful.stack@gmail.com>
2022-03-14 00:41:05 +0800

    convert phn to readable vocab

--- telephone/main.py
+++ telephone/main.py
@@ -18,9 +18,81 @@ class Telephonemizer:
             logits = self.model(input_values).logits
 
         predicted_ids = torch.argmax(logits, dim=-1)
-        return self.processor.batch_decode(predicted_ids)[0].replace(" ", "")
+        phonemes = self.processor.batch_decode(predicted_ids)[0].replace(" ", "")
+        for p, w in _phn2word_mapping_table.items():
+            phonemes = phonemes.replace(p, w)
+        return phonemes
 
     def convert_text(self, text):
         phonemes = self.backend.phonemize([text], separator=self.separator)
         phonemes = phonemes[0].strip()
+        for p, w in _phn2word_mapping_table.items():
+            phonemes = phonemes.replace(p, w)
         return phonemes
+
+
+_phn2word_mapping_table = {'ð': 'T',
+                           'ə': '@',
+                           'ʌ': 'u',
+                           'v': 'v',
+                           'æ': 'a',
+                           'n': 'n',
+                           'd': 'd',
+                           'ɪ': 'i',
+                           't': 't',
+                           'uː': 'U',
+                           'eɪ': 'A',
+                           'w': 'w',
+                           'z': 'z',
+                           'ɔ': 'o',
+                           'f': 'f',
+                           'ɔːɹ': 'Wr',
+                           'ɹ': 'r',
+                           'ɔː': 'W',
+                           'b': 'b',
+                           'aɪ': 'I',
+                           'h': 'h',
+                           'iː': 'E',
+                           'i': 'E',
+                           'm': 'm',
+                           'ɜː': '3',
+                           'tʃ': 'tS',
+                           'ʃ': 'S',
+                           's': 's',
+                           'ɑːɹ': '~r',
+                           'ɑː': '~',
+                           'ɛɹ': 'er',
+                           'ɛ': 'e',
+                           'ɚ': 'er',
+                           'l': 'l',
+                           'oʊ': 'O',
+                           'ʊ': 'U',
+                           'ʊɹ': 'Ur',
+                           'ŋ': 'N',
+                           'oːɹ': 'Or',
+                           'oː': 'O',
+                           'ɡ': 'g',
+                           'ɾ': 'd',
+                           'p': 'p',
+                           'θ': 'D',
+                           'ɐ': '@',
+                           'aʊ': '^U',
+                           'ᵻ': 'i',
+                           'j': 'j',
+                           'ɪɹ': 'ir',
+                           'k': 'k',
+                           'əl': '@l',
+                           'iə': 'E@',
+                           'dʒ': 'dZ',
+                           'ʒ': 'Z',
+                           'ɔɪ': 'oi',
+                           'ʔ': 'ʔ',
+                           'n̩': 'n',
+                           'aɪɚ': 'Ier',
+                           'aɪə': 'I@',
+                           'iːː': 'E',
+                           'x': 'H',
+                           'r': 'r',
+                           'ɑ̃': 'A',
+                           'ɡʲ': 'g'
+                           }
